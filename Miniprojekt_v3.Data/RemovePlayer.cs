cf9f6653173b1ed21811ed70f9446c803c96607b
Plupp86 <plupp86@gmail.com>
2018-02-02 11:56:40 +0100

    Added remove player feature

---
+++ Miniprojekt_v3.Data/RemovePlayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Miniprojekt_v3.Data
+{
+	public partial class DataManager
+	{
+
+		public void RemovePlayer(int id)
+		{
+			SqlCommand command = new SqlCommand($"DELETE FROM Players WHERE Id = {id}", conn);
+			
+			
+			try
+			{
+				conn.Open();
+				command.ExecuteNonQuery();
+			}
+			catch
+			{
+
+				throw;
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
+
+
+
+	}
+}
